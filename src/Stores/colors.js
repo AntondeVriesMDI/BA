21bd8c0026f47f04e1242d1564066a9f5256a18e
Anton de Vries <vries@imis.uni-luebeck.de>
2023-05-11 17:54:01 +0200

    fixed Import Error

--- src/Stores/colors.js
+++ src/Stores/colors.js
@@ -1,8 +1,7 @@
-
-import { derived } from 'svelte/store';
+import { derived } from "svelte/store";
 import { scaleLinear } from "d3-scale";
-import { currentDataPoint, getPercDist} from '../Stores/datapoints.js';
-import { currentEce } from '../Stores/metrics.js';
+import { currentDataPoint, getPercDist } from "../Stores/dataPoints.js";
+import { currentEce } from "../Stores/metrics.js";
 
 /**
  * Berechnet die Farbbewwertung der Displays
@@ -10,38 +9,47 @@ import { currentEce } from '../Stores/metrics.js';
 
 //Farben des Gradienten in RGB
 const COLORS = [
-    { name: "green" , percent: 0, r: 46, g: 229, b: 0 },
-    { name: "yellow", percent: 50, r: 255, g: 246, b: 82 },
-    { name: "red" , percent: 110, r: 255,g: 82, b: 82}
-  ]
+  { name: "green", percent: 0, r: 46, g: 229, b: 0 },
+  { name: "yellow", percent: 50, r: 255, g: 246, b: 82 },
+  { name: "red", percent: 110, r: 255, g: 82, b: 82 },
+];
 
 /**
  * Akutelle Farbe (Fabrbewertungen der Displays)
  */
- export const currentColor = derived( [currentDataPoint, currentEce] , ([$currentDataPoint,$currentEce]) =>{
+export const currentColor = derived(
+  [currentDataPoint, currentEce],
+  ([$currentDataPoint, $currentEce]) => {
     return getColor(100 - getPercDist(0.7, 0.88, $currentEce));
-  });
+  }
+);
 
-  /**
+/**
  * Ermittelt aktuelle Farbbewertungen
  * @param {} percDist prozentuale Distanz zwischen aktueller ECE und höchstmöglicher ECE
  * @returns Farbe in RGB
  */
-export const getColor = function(percDist){
-    let domainstart = COLORS[0];
-    let domainend = COLORS[1];
+export const getColor = function (percDist) {
+  let domainstart = COLORS[0];
+  let domainend = COLORS[1];
 
-    if(percDist > 50){
-      domainstart = COLORS[1];
-      domainend = COLORS[2];
-    }
-    const rScale = scaleLinear().domain([domainstart.percent, domainend.percent]).range([domainstart.r, domainend.r]);
-    const gScale = scaleLinear().domain([domainstart.percent, domainend.percent]).range([domainstart.g, domainend.g]);
-    const bScale = scaleLinear().domain([domainstart.percent, domainend.percent]).range([domainstart.b, domainend.b]);
-
-    return {
-      r: rScale(percDist),
-      g: gScale(percDist),
-      b: bScale(percDist),
-    }
+  if (percDist > 50) {
+    domainstart = COLORS[1];
+    domainend = COLORS[2];
   }
+  const rScale = scaleLinear()
+    .domain([domainstart.percent, domainend.percent])
+    .range([domainstart.r, domainend.r]);
+  const gScale = scaleLinear()
+    .domain([domainstart.percent, domainend.percent])
+    .range([domainstart.g, domainend.g]);
+  const bScale = scaleLinear()
+    .domain([domainstart.percent, domainend.percent])
+    .range([domainstart.b, domainend.b]);
+
+  return {
+    r: rScale(percDist),
+    g: gScale(percDist),
+    b: bScale(percDist),
+  };
+};
